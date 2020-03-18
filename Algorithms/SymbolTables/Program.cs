1694946da3d538fbcd5805d7dce8a3e6d94b94bd
durgaprsd04 <durgaprsd04@gmail.com>
2020-03-18 23:10:43 +0530

    Node implementation of Symbol tables

--- Algorithms/SymbolTables/Program.cs
+++ Algorithms/SymbolTables/Program.cs
@@ -13,15 +13,29 @@ namespace SymbolTables
         {
             Console.WriteLine("Hello World!");
             
-            BinarySearchTree bst = new BinarySearchTree();
-
+            //BinarySearchTree bst = new BinarySearchTree();
+            ST st = new ST();
+        
             /* count positions */
         
             Console.WriteLine("Enter String");
-            var entry = Console.ReadLine();
-            //for(int i =0;i<entry.Length;i++)
-            //    s1.Add( entry[i].ToString(),i);
-            //s1.Print();
+            var entry = "helloworld";//Console.ReadLine();
+            for(int i =0;i<entry.Length;i++)
+               st.Add( (char)entry[i],i+1);
+            st.Print();
+            Console.WriteLine("Search of o is {0}",st.Search('o'));
+            Console.WriteLine("Size of the ST is "+ st.Size());
+            Console.WriteLine("Removal of o is {0}",st.Remove('o'));
+            st.Print();
+            Console.WriteLine("Removal of a is {0}",st.Remove('a'));
+            st.Print();
+            Console.WriteLine("Size of the ST is "+ st.Size());
+            var v = st.Keys();
+            foreach(var c in v)
+            {
+                Console.WriteLine(c.ToString());
+            }
+                //s1.Print();
             /* count occurences */
             /*
             for(int i =0;i<entry.Length;i++)
@@ -30,11 +44,11 @@ namespace SymbolTables
                     else
                         s1.Add( entry[i].ToString(),1);
              */
-            for(int i =0;i<entry.Length;i++)
-                {
-                bst.Add(entry[i].ToString());
-                }
-            bst.Print();
+            //for(int i =0;i<entry.Length;i++)
+              //  {
+                //bst.Add(entry[i].ToString());
+                //}
+            //bst.Print();
         }
     }
 }
