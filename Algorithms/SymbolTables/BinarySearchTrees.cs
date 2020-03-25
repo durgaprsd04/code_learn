76b59e5567e18b5cabb5fdeae58be25b93bee2d3
durgaprsd04 <durgaprsd04@gmail.com>
2020-03-25 11:00:01 +0530

    all files commented

--- Algorithms/SymbolTables/BinarySearchTrees.cs
+++ Algorithms/SymbolTables/BinarySearchTrees.cs
@@ -1,182 +1,182 @@
-using System;
-namespace SymbolTables
-{
-    public class Node
-    {
-        public string value;
-        public Node left;
-        public Node right;
-    }
-    public class BinarySearchTree
-    { 
-        private Node root;
-        public void Add(string key)
-        {
-           AddItem(ref root, key);
+// using System;
+// namespace SymbolTables
+// {
+//     public class Node
+//     {
+//         public string value;
+//         public Node left;
+//         public Node right;
+//     }
+//     public class BinarySearchTree
+//     { 
+//         private Node root;
+//         public void Add(string key)
+//         {
+//            AddItem(ref root, key);
                 
-        }
-        private void AddItem(ref Node root,string key)
-        {
-             if(root==null)
-            {
-                //Console.WriteLine("here in adding item"+key);
-                root = new Node();
-                root.value=key;
-            }
-            else
-            {
-                if(string.Compare(root.value,key)>0)
-                {
-                    //Console.WriteLine("Adding {0} to left of {1}",key,root.value);
-                    AddItem(ref root.left,key);
+//         }
+//         private void AddItem(ref Node root,string key)
+//         {
+//              if(root==null)
+//             {
+//                 //Console.WriteLine("here in adding item"+key);
+//                 root = new Node();
+//                 root.value=key;
+//             }
+//             else
+//             {
+//                 if(string.Compare(root.value,key)>0)
+//                 {
+//                     //Console.WriteLine("Adding {0} to left of {1}",key,root.value);
+//                     AddItem(ref root.left,key);
                     
-                }
-                else
-                {
-                    //Console.WriteLine("Adding {0} to right {1}",key, root.value);
-                    AddItem(ref root.right, key);
+//                 }
+//                 else
+//                 {
+//                     //Console.WriteLine("Adding {0} to right {1}",key, root.value);
+//                     AddItem(ref root.right, key);
                     
-                }
-            }
-        }
-        public bool Search(string key)
-        {
-            return Search(root, key);
+//                 }
+//             }
+//         }
+//         public bool Search(string key)
+//         {
+//             return Search(root, key);
 
             
-        }
-        private bool Search(Node node, string key)
-        {
-            if(node==null)
-                return false;
-            if(node.value==key)
-                return true;
+//         }
+//         private bool Search(Node node, string key)
+//         {
+//             if(node==null)
+//                 return false;
+//             if(node.value==key)
+//                 return true;
             
-            bool i=false;
-            if(node.left!=null)
-                i= Search(node.left,key);
-            if(node.right!=null)
-                i = i||  Search(node.right,key);
-            return i;
-        }
-        public  string Max()
-        {
-            return Max(root);
-        }
-        private string Max(Node node)
-        {
-            string max = string.Empty,leftmax=string.Empty,rightmax=string.Empty;
-            if(node==null)
-                return string.Empty;
-            max=node.value;
-            if(node.left!=null)
-            {
-                leftmax=Max(node.left);
-                //Console.WriteLine(leftmax);
-                max = (string.Compare(node.value, leftmax)>0)?node.value:leftmax;
-            }   
-            if(node.right!=null)pointer=1;``
-            {
-                rightmax=Max(node.right);
-                //Console.WriteLine(rightmax);
-                max = (string.Compare(leftmax, rightmax)>0)?leftmax:rightmax;
-            }
-            //Console.WriteLine(max);
-            return max;
-        }
-        public string Min()
-        {
-            return Min(root);
-        }
-        public string Min(Node node)
-        {
-            string min = string.Empty,leftmin=string.Empty,rightmin=string.Empty;
-            if(node==null)
-                return null;
-            min=node.value;
-            if(node.left!=null)
-            {
-                leftmin=Min(node.left);
-                //Console.WriteLine(leftmin);
-                min = (leftmin!=null)?node.value:leftmin;
-            }   
-            if(node.right!=null)
-            {
-                rightmin=Min(node.right);
-                //Console.WriteLine(rightmin);
-                min = (string.Compare(leftmin, rightmin)>0)?leftmin:rightmin;
-            }
-            Console.WriteLine(min);
-            return min;
-        }
-        public string Ceil(string key)
-        {
-            return string.Empty;
-        }
-        public string Floor(string key)
-        {
-            return string.Empty;
-        }
-        public int Rank(string key)
-        {
-            return 0;
-        }
-        public int Size(string key)
-        {
-            return Size(root, key,false);
-        }
-        private int Size(Node root, string key, bool isFound)
-        {
-            int size=0;
-            if(root==null)
-            {
-                return 0;
-            }
-            //Console.Write(root.value);
-            if(root.value==key || isFound)
-            {   
-                //Console.WriteLine("insert "+root.value+"key "+key);
-                if(root.left!=null)
-                    size+=(1+Size(root.left,key, true));   
-                if(root.right!=null)
-                    size+=(1+Size(root.right,key, true));  
-                return size; 
-            }
-            else
-            {
-            if(root.left!=null)
-                size+=Size(root.left,key, false);
-            if(root.right!=null)
-                size+=Size(root.right,key, false);
-            return size;
-            }
+//             bool i=false;
+//             if(node.left!=null)
+//                 i= Search(node.left,key);
+//             if(node.right!=null)
+//                 i = i||  Search(node.right,key);
+//             return i;
+//         }
+//         public  string Max()
+//         {
+//             return Max(root);
+//         }
+//         private string Max(Node node)
+//         {
+//             string max = string.Empty,leftmax=string.Empty,rightmax=string.Empty;
+//             if(node==null)
+//                 return string.Empty;
+//             max=node.value;
+//             if(node.left!=null)
+//             {
+//                 leftmax=Max(node.left);
+//                 //Console.WriteLine(leftmax);
+//                 max = (string.Compare(node.value, leftmax)>0)?node.value:leftmax;
+//             }   
+//             if(node.right!=null)
+//             {
+//                 rightmax=Max(node.right);
+//                 //Console.WriteLine(rightmax);
+//                 max = (string.Compare(leftmax, rightmax)>0)?leftmax:rightmax;
+//             }
+//             //Console.WriteLine(max);
+//             return max;
+//         }
+//         public string Min()
+//         {
+//             return Min(root);
+//         }
+//         public string Min(Node node)
+//         {
+//             string min = string.Empty,leftmin=string.Empty,rightmin=string.Empty;
+//             if(node==null)
+//                 return null;
+//             min=node.value;
+//             if(node.left!=null)
+//             {
+//                 leftmin=Min(node.left);
+//                 //Console.WriteLine(leftmin);
+//                 min = (leftmin!=null)?node.value:leftmin;
+//             }   
+//             if(node.right!=null)
+//             {
+//                 rightmin=Min(node.right);
+//                 //Console.WriteLine(rightmin);
+//                 min = (string.Compare(leftmin, rightmin)>0)?leftmin:rightmin;
+//             }
+//             Console.WriteLine(min);
+//             return min;
+//         }
+//         public string Ceil(string key)
+//         {
+//             return string.Empty;
+//         }
+//         public string Floor(string key)
+//         {
+//             return string.Empty;
+//         }
+//         public int Rank(string key)
+//         {
+//             return 0;
+//         }
+//         public int Size(string key)
+//         {
+//             return Size(root, key,false);
+//         }
+//         private int Size(Node root, string key, bool isFound)
+//         {
+//             int size=0;
+//             if(root==null)
+//             {
+//                 return 0;
+//             }
+//             //Console.Write(root.value);
+//             if(root.value==key || isFound)
+//             {   
+//                 //Console.WriteLine("insert "+root.value+"key "+key);
+//                 if(root.left!=null)
+//                     size+=(1+Size(root.left,key, true));   
+//                 if(root.right!=null)
+//                     size+=(1+Size(root.right,key, true));  
+//                 return size; 
+//             }
+//             else
+//             {
+//             if(root.left!=null)
+//                 size+=Size(root.left,key, false);
+//             if(root.right!=null)
+//                 size+=Size(root.right,key, false);
+//             return size;
+//             }
             
-            //Console.WriteLine("not so impossible"+root.value+":"+key);
-            //return 0;
-        }
-        public void Print()
-        {
-            //Console.WriteLine("Size of h"+Size("q"));
-            //Console.WriteLine("The element x exists in "+ Search("y"));
-            Console.WriteLine("The max element is {0}", Max());
-            Print(root, '?');
-        }
-        private void Print(Node n , char j)
-        {
-            if(n==null)
-            {       
-                //Console.WriteLine("Weird line");
-                return;
-            }
-            //Console.WriteLine( Size(n.value));
-            Console.Write(new String(j,1+Size(n.value))+n.value);
-            Console.WriteLine();
-            if(n.left!=null)
-                Print(n.left, '>');
-            if(n.right!=null)
-                Print(n.right, '<');
+//             //Console.WriteLine("not so impossible"+root.value+":"+key);
+//             //return 0;
+//         }
+//         public void Print()
+//         {
+//             //Console.WriteLine("Size of h"+Size("q"));
+//             //Console.WriteLine("The element x exists in "+ Search("y"));
+//             Console.WriteLine("The max element is {0}", Max());
+//             Print(root, '?');
+//         }
+//         private void Print(Node n , char j)
+//         {
+//             if(n==null)
+//             {       
+//                 //Console.WriteLine("Weird line");
+//                 return;
+//             }
+//             //Console.WriteLine( Size(n.value));
+//             Console.Write(new String(j,1+Size(n.value))+n.value);
+//             Console.WriteLine();
+//             if(n.left!=null)
+//                 Print(n.left, '>');
+//             if(n.right!=null)
+//                 Print(n.right, '<');
             
 
-        }
-    }
-}
+//         }
+//     }
+// }
--- Algorithms/SymbolTables/SymbolTablesArray.cs
+++ Algorithms/SymbolTables/SymbolTablesArray.cs
@@ -1,126 +1,165 @@
-using System;
-namespace SymbolTables
-{
-public class STNew<T1,T2>
-{
+// // using System;
+// // namespace SymbolTables
+// // {
+// // public class STNew<T1,T2>
+// // {
 
-    private const int INIT_COUNT=1;
-    ArrayHelper<T1> t1 = new ArrayHelper<T1>();
-    ArrayHelper<T2> t2 = new ArrayHelper<T2>();
+// //     private const int INIT_COUNT=1;
+// //     ArrayHelper<T1> t1 = new ArrayHelper<T1>();
+// //     ArrayHelper<T2> t2 = new ArrayHelper<T2>();
 
-    private T1 [] t1Array = new T1[INIT_COUNT];
-    private T2 [] t2Array = new T2 [INIT_COUNT];
-    private int count=0;
+// //     private T1 [] t1Array = new T1[INIT_COUNT];
+// //     private T2 [] t2Array = new T2 [INIT_COUNT];
+// //     private int count=0;
 
-    public void Add(T1 a, T2 b)
-    {
-        if(count >= t1Array.Length)
-        {
-            T1 [] t1ArrayNew = new T1[4*count];
-            for(int i=0;i<t1Array.Length;i++)
-            {
-                t1ArrayNew[i]=t1Array[i];
-            }
-            t1Array= t1ArrayNew;
-            T2 [] t2ArrayNew = new T2[4*count];
-            for(int i=0;i<t2Array.Length;i++)
-            {
-                t2ArrayNew[i]=t2Array[i];
-            }
-            t2Array= t2ArrayNew;
-        }
-        int c=-1;
-        if(count>=1){ c= t1.HasElement(t1Array, a);}
-        if(c==-1)
-        {
-            t1Array[count]=a;
-            t2Array[count]=b;
-            count++;
-        }
-        else
-        {
-            //Console.WriteLine(c);
-            t2Array[c]=b;   
-        }
+// //     public void Add(T1 a, T2 b)
+// //     {
+// //         if(count >= t1Array.Length)
+// //         {
+// //             T1 [] t1ArrayNew = new T1[4*count];
+// //             for(int i=0;i<t1Array.Length;i++)
+// //             {
+// //                 t1ArrayNew[i]=t1Array[i];
+// //             }
+// //             t1Array= t1ArrayNew;
+// //             T2 [] t2ArrayNew = new T2[4*count];
+// //             for(int i=0;i<t2Array.Length;i++)
+// //             {
+// //                 t2ArrayNew[i]=t2Array[i];
+// //             }
+// //             t2Array= t2ArrayNew;
+// //         }
+// //         int c=-1;
+// //         if(count>=1){ c= t1.HasElement(t1Array, a);}
+// //         if(c==-1)
+// //         {
+// //             t1Array[count]=a;
+// //             t2Array[count]=b;
+// //             count++;
+// //         }
+// //         else
+// //         {
+// //             //Console.WriteLine(c);
+// //             t2Array[c]=b;   
+// //         }
         
-    }
-    public bool HasElement(T1 a)
-    {
-        Console.WriteLine(t1.HasElement(t1Array, a));
-        return t1.HasElement(t1Array, a)!=-1?true:false;
-    }
-    public T2 GetElement(T1 a)
-    {
-        var c1=t1.HasElement(t1Array, a);
-        if(c1!=-1)
-            return t2Array [c1];
-        else
-            return default(T2);
-    }
-    public void Print()
-    {
-        for(int i=0;i<count;i++)
-        {
-            Console.WriteLine("{0} : {1}",t1Array[i],t2Array[i]);
-        }
-    }
-    public void Remove(T1 a)
-    {
-        int i;
-        for( i=0;i<t1Array.Length;i++)
-            if(t1Array[i].Equals(a))
-                break;
+// //     }
+// //     public bool HasElement(T1 a)
+// //     {
+// //         Console.WriteLine(t1.HasElement(t1Array, a));
+// //         return t1.HasElement(t1Array, a)!=-1?true:false;
+// //     }
+// //     public T2 GetElement(T1 a)
+// //     {
+// //         var c1=t1.HasElement(t1Array, a);
+// //         if(c1!=-1)
+// //             return t2Array [c1];
+// //         else
+// //             return default(T2);
+// //     }
+// //     public void Print()
+// //     {
+// //         for(int i=0;i<count;i++)
+// //         {
+// //             Console.WriteLine("{0} : {1}",t1Array[i],t2Array[i]);
+// //         }
+// //     }
+// //     public void Remove(T1 a)
+// //     {
+// //         int i;
+// //         for( i=0;i<t1Array.Length;i++)
+// //             if(t1Array[i].Equals(a))
+// //                 break;
         
-        t1.RemoveAt(t1Array, i);
-        t2.RemoveAt(t2Array,i);
-        count--;
-        /* if(count<t1Array.Length/4)
-        {
-            T1 [] t1ArrayNew = new T1[4*count];
-            for(int i=0;i<t1Array.Length;i++)
-            {
-                t1ArrayNew[i]=t1Array[i];
-            }
-            t1Array= t1ArrayNew;
-            T2 [] t2ArrayNew = new T2[4*count];
-            for(int i=0;i<t2Array.Length;i++)
-            {
-                t2ArrayNew[i]=t2Array[i];
-            }
-            t2Array= t2ArrayNew;
-        } */
+// //         t1.RemoveAt(t1Array, i);
+// //         t2.RemoveAt(t2Array,i);
+// //         count--;
+// //         /* if(count<t1Array.Length/4)
+// //         {
+// //             T1 [] t1ArrayNew = new T1[4*count];
+// //             for(int i=0;i<t1Array.Length;i++)
+// //             {
+// //                 t1ArrayNew[i]=t1Array[i];
+// //             }
+// //             t1Array= t1ArrayNew;
+// //             T2 [] t2ArrayNew = new T2[4*count];
+// //             for(int i=0;i<t2Array.Length;i++)
+// //             {
+// //                 t2ArrayNew[i]=t2Array[i];
+// //             }
+// //             t2Array= t2ArrayNew;
+// //         } */
         
-    }   
-}
-public class ArrayHelper<T3>
-{
-    public  void  RemoveAt(T3 [] a, int i)
-    {
-        T3 [] a1 = new T3 [i];
-        for(int j=0;j<a.Length;j++)
-            if(i!=j)
-                a1[j]=a[j];   
-        a=a1;
-    }
-    public int HasElement(T3 [] a, T3 j)
-    {
-        //Console.WriteLine(a.Length);
-        int i;
-        for( i=0;i<a.Length;i++)
-        {
-            if(a[i]==null)
-            {
-                i=-1;
-                break;
-            }
-            if(a[i].Equals(j))
-                break;
-        }
-        if(i<a.Length)
-            return i;
-        else
-            return -1;
-    }
-}
+// //     }   
+// // }
+// // public class ArrayHelper<T3>
+// // {
+// //     public  void  RemoveAt(T3 [] a, int i)
+// //     {
+// //         T3 [] a1 = new T3 [i];
+// //         for(int j=0;j<a.Length;j++)
+// //             if(i!=j)
+// //                 a1[j]=a[j];   
+// //         a=a1;
+// //     }
+// //     public int HasElement(T3 [] a, T3 j)
+// //     {
+// //         //Console.WriteLine(a.Length);
+// //         int i;
+// //         for( i=0;i<a.Length;i++)
+// //         {
+// //             if(a[i]==null)
+// //             {
+// //                 i=-1;
+// //                 break;
+// //             }
+// //             if(a[i].Equals(j))
+// //                 break;
+// //         }
+// //         if(i<a.Length)
+// //             return i;
+// //         else
+// //             return -1;
+// //     }
+// // }
 
-}
+// // }
+
+// using System;
+// namespace SymbolTables
+// {
+//     private class Node
+//     {
+//         char key;
+//         char value;
+//          Node(char key, char value)
+//         {
+//             this.key=key;
+//             this.value=value;
+
+//         }
+//         Node Next;
+//     }
+//     public class ST
+//     {
+//         private Node head;
+//         public void Add(char key, char value)
+//         {
+            
+//             if(head==null)
+//                 head= new Node(key, value);
+//             if(head!=null)
+//                 Add(head, new Node(key,value));
+//         }
+//         private void Add(Node parent, Node child)
+//         {
+//             if(parent.key==child.key)
+//                 parent.value=child.value;
+//             if(parent.Next==null)
+//                 parent.Node=child;
+//             if(parent.Next!=null)
+//                 Add(parent.Next,child);
+
+//         }
+//     }
+// }
