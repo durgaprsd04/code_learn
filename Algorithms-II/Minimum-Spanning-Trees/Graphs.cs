958f5b5593b2b6ae088eff59517d3537b650f344
durgaprsd04 <durgaprsd04@gmail.com>
2020-05-25 23:05:25 +0530

    all files for MST

---
+++ Algorithms-II/Minimum-Spanning-Trees/Graphs.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Collections.Generic;
+
+namespace Minimum_Spanning_Trees
+{
+    public class Edge : IComparable<Edge>
+    {
+        double weight;
+        int vertice;
+        int other;
+        bool isblack;
+
+        public bool Isblack { get => isblack; set => isblack = value; }
+        public int Vertice { get => vertice; set => vertice = value; }
+        public int Other { get => other; set => other = value; }
+
+        public Edge(double weight, int vertice, int other)
+        {
+            this.weight = weight;
+            this.Vertice= vertice;
+            this.Other = other;
+        }
+        public override string ToString()
+        {
+            return string.Format("{0}->{1}({2} black -{3})",vertice, Other, weight,isblack);
+        }
+
+        public int CompareTo(Edge e)
+        {
+            return (int)(100*(this.weight - e.weight));
+        }
+    }
+    public class Graph
+    {
+        Bag<Edge> []  edges ;
+        List<Edge> EdgeList = new List<Edge>();
+        int vertices,edgeCount;
+        bool [] marked;
+        public Graph(int vertices, int edgeCount)
+        {
+            this.Vertices= vertices;
+            this.edgeCount= edgeCount;
+            edges = new Bag<Edge>[vertices];
+            marked  = new bool[vertices];
+          
+        }
+
+        public int Vertices { get => vertices; set => vertices = value; }
+        public List<Edge> EdgeList1 { get => EdgeList; set => EdgeList = value; }
+
+        public void AddEdge(double weight, int to, int from)
+        {
+            var edge = new Edge(weight, to, from);
+            EdgeList1.Add(edge);
+            if(edges[to]==null)
+                edges[to]= new Bag<Edge>(edge);
+            else
+                edges[to].Add(edge);
+            var edge1 = new Edge(weight, from, to);
+            if(edges[from]==null)
+                edges[from]= new Bag<Edge>(edge1);
+            else
+                edges[from].Add(edge1);
+            
+        }
+        public void Print()
+        {
+            for(int i =0;i<Vertices;i++)
+            {
+              if(edges[i]!=null)
+              {
+                  Console.Write("[{0}]",i);
+                  edges[i].Print();
+              }
+                 
+             else
+             Console.WriteLine("[{0}]",i);
+            }
+        }
+        public void PrintEdgeList()
+        {
+            EdgeList1.Sort();
+            foreach(var edge in EdgeList1)
+            {
+                Console.WriteLine(edge.ToString());
+            }
+        }
+    }
+}
---
+++ Algorithms-II/Minimum-Spanning-Trees/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace Minimum_Spanning_Trees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Reading tinyG3.txt");
+            StreamReader sr = new StreamReader("tinyG3.txt");
+            var vertices = Convert.ToInt32(sr.ReadLine());
+            var edges = Convert.ToInt32(sr.ReadLine());
+            Graph graph =new Graph(vertices, edges);
+            for(int i=0;i<edges;i++)
+            {
+                var v  =sr.ReadLine().Split('-');
+                var vertice1= Convert.ToInt32(v[0]);
+                var vertice2 = Convert.ToInt32(v[1]);
+                var weight = Convert.ToDouble(v[2]);
+                graph.AddEdge(weight, vertice1, vertice2);
+            }
+            Console.WriteLine("Graph created");
+            graph.Print();
+            graph.PrintEdgeList();
+            KruskalAlgorithm ka = new KruskalAlgorithm(graph);
+            ka.Run();
+            graph.PrintEdgeList();
+        }
+    }
+}
