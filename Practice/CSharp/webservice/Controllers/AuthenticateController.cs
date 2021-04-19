79b8839d3bd7660082e18d7bd7b45c61066e1ca0
durgaprsd04 <durgaprsd04@gmail.com>
2021-04-19 18:57:32 +0530

    miinor changes

---
+++ Practice/CSharp/webservice/Controllers/AuthenticateController.cs
@@ -0,0 +1,71 @@
+using System;  
+using System.Collections.Generic;  
+using System.ComponentModel.DataAnnotations;  
+using System.IdentityModel.Tokens.Jwt;  
+using System.Linq;   
+using System.Text;  
+using System.Threading.Tasks;  
+using Microsoft.AspNetCore.Authentication;  
+using Microsoft.AspNetCore.Authorization;  
+using Microsoft.AspNetCore.Mvc;  
+using Microsoft.Extensions.Configuration;  
+using Microsoft.IdentityModel.Tokens;  
+  
+namespace webservice.Controllers
+{  
+    public class AuthenticateController : BaseController  
+    {  
+        private IConfiguration _config;  
+        public AuthenticateController(IConfiguration config)  
+        {  
+            _config = config;  
+        } 
+        private string GenerateJSONWebToken(LoginModel userInfo)  
+        {  
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));  
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);  
+  
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"],  
+              _config["Jwt:Issuer"],  
+              null,  
+              expires: DateTime.Now.AddMinutes(120),  
+              signingCredentials: credentials);  
+  
+            return new JwtSecurityTokenHandler().WriteToken(token);  
+        }
+        private async Task<LoginModel> AuthenticateUser(LoginModel login)  
+        {  
+            LoginModel user = null;  
+  
+            //Validate the User Credentials      
+            //Demo Purpose, I have Passed HardCoded User Information      
+            if (login.UserName == "Jay")  
+            {  
+                user =  new LoginModel { UserName = "Jay", Password = "123456" };  
+            }  
+            return user;  
+        } 
+        [AllowAnonymous]  
+        [HttpPost(nameof(Login))]  
+        public async Task<IActionResult> Login([FromBody] LoginModel data)  
+        {  
+            IActionResult response = Unauthorized();  
+            var user = await AuthenticateUser(data);  
+            if (data != null)  
+            {  
+                var tokenString = GenerateJSONWebToken(user);  
+                response = Ok(new { Token = tokenString , Message = "Success" });  
+            }  
+            return response;  
+        }   
+        [HttpGet(nameof(Get))]  
+        public async Task<IEnumerable<string>> Get()  
+        {  
+            var accessToken = await HttpContext.GetTokenAsync("access_token");  
+  
+            return new string[] { accessToken };  
+        }  
+    }
+    
+}
+
--- Practice/CSharp/webservice/Startup.cs
+++ Practice/CSharp/webservice/Startup.cs
@@ -4,12 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using System.Text;
+using Microsoft.IdentityModel.Tokens ;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.OpenApi.Models;
 
 namespace webservice
@@ -28,10 +28,58 @@ namespace webservice
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "webservice", Version = "v1" });
+            services.AddSwaggerGen(swagger =>  
+            {  
+                //This is to generate the Default UI of Swagger Documentation  
+                swagger.SwaggerDoc("v1", new OpenApiInfo  
+                {   
+                    Version= "v1",   
+                    Title = "JWT Token Authentication API",  
+                    Description="ASP.NET Core 3.1 Web API" });  
+                // To Enable authorization using Swagger (JWT)  
+                swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()  
+                {  
+                    Name = "Authorization",  
+                    Type = SecuritySchemeType.ApiKey,  
+                    Scheme = "Bearer",  
+                    BearerFormat = "JWT",  
+                    In = ParameterLocation.Header,  
+                    Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",  
+                });
+                swagger.AddSecurityRequirement(new OpenApiSecurityRequirement  
+                {  
+                    {  
+                          new OpenApiSecurityScheme  
+                            {  
+                                Reference = new OpenApiReference  
+                                {  
+                                    Type = ReferenceType.SecurityScheme,  
+                                    Id = "Bearer"  
+                                }  
+                            },  
+                            new string[] {}  
+  
+                    }  
+                });
             });
+            services.AddAuthentication(option =>  
+            {  
+                option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;  
+                option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;  
+  
+            }).AddJwtBearer(options =>  
+            {  
+                options.TokenValidationParameters = new TokenValidationParameters  
+                {  
+                    ValidateIssuer = true,  
+                    ValidateAudience = true,  
+                    ValidateLifetime = false,  
+                    ValidateIssuerSigningKey = true,  
+                    ValidIssuer = Configuration["Jwt:Issuer"],  
+                    ValidAudience = Configuration["Jwt:Issuer"],  
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"])) //Configuration["JwtToken:SecretKey"]  
+                };  
+            }); 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,7 +89,7 @@ namespace webservice
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "webservice v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "webservice mine v1"));
             }
 
             app.UseHttpsRedirection();
